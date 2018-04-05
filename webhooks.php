43023e1eb67ec9cb6e2778c7d7e3b1c21d986a8c
Piyaput <piyaput@thinknet.co.th>
2018-04-05 14:38:43 +0700

    test

--- webhooks.php
+++ webhooks.php
@@ -62,6 +62,54 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
+
+		else if ($event['type'] != 'message' || $event['message']['type'] != 'text') {
+			// Get text sent
+			// $text = $event['source']['userId'];
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			$TextInput = $event['message']['text'];
+
+			if($TextInput == 'รู้จักกับเรา') {
+				$text = '';
+				break;
+			} else if($TextInput == 'A') {
+				$text = 'URL A';
+			}
+
+			$text = '';
+			foreach ($event as $key => $value) {
+				$text .= 'Key => '.$key.' , Value => '.$value." ## ";
+			}
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
 	}
 }
 
